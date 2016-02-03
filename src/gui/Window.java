c9288eb5eda6db22a13140e73f9c865f079b04d6
venegas <venegas@iastate.edu>
2016-02-02 20:44:50 -0600

    Added Some Train Functionality To GUI
    I went ahead and messed around with the GUI you know Just for funsies.
    I went ahead and tried to implement the train, so you could actually
    try and train. I haven’t actually tested the it, but feel free to give
    it a try.

--- src/gui/Window.java
+++ src/gui/Window.java
@@ -7,6 +7,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
@@ -15,7 +19,10 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import binding.OpenCVBinding;
 
 /**
  * GUI to handle image selection and calling of processing commands
@@ -90,13 +97,43 @@ public class Window extends JFrame {
 	private void train() {
 		//TODO
 		JFileChooser openFile = new JFileChooser();
-		openFile.setMultiSelectionEnabled(true);
-		openFile.showOpenDialog(null);
 		
+		
+		//Have the user enter the name
+		String userInput = JOptionPane.showInputDialog(getContentPane(), "Enter the Name of the Student", "User Input", JOptionPane.INFORMATION_MESSAGE);
+		
+		//check if they entered something, and if not do nothing
+		if(userInput != null){
+			openFile.setMultiSelectionEnabled(true);
+			openFile.showOpenDialog(null);
+			
 		// Create a map using the file paths of selected images and send to training functions
 		if(openFile.getSelectedFiles() != null) {
+			
 			printFilePaths(openFile.getSelectedFiles());
+			putFilesInList(openFile.getSelectedFiles());
+			
+			printf(userInput);
+			Map<String,List<String>> m = new HashMap<String,List<String>>();
+			m.put(userInput, putFilesInList(openFile.getSelectedFiles()));
+			printf(m.get(userInput).toString());
+			
+			//Do the training
+			OpenCVBinding.Result result = OpenCVBinding.trainFiles(m);
+			
+			if(!(result.success)){
+				JOptionPane.showMessageDialog(getContentPane(), result.errors.toString(), "Error", JOptionPane.ERROR_MESSAGE);
+			}else{
+				JOptionPane.showMessageDialog(getContentPane(), "Trained Successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
+			}
+			
+		}else{
+			JOptionPane.showMessageDialog(this, "Null File", "Error", JOptionPane.ERROR_MESSAGE);
 		}
+		}
+		
+		
+		
 
 	}
 	
@@ -108,6 +145,17 @@ public class Window extends JFrame {
 		for (int i = 0; i < files.length; i++) {
 			System.out.println(files[i].getAbsolutePath());
 		}
+	}
+	
+	
+private ArrayList<String> putFilesInList(File[] files){
+		ArrayList<String> list = new ArrayList<String>();
+		
+		for(int i = 0; i < files.length; i++){
+		list.add(files[i].getAbsolutePath());	
+		}
+		
+		return list;
 	}
 	
 	/**
@@ -147,6 +195,14 @@ public class Window extends JFrame {
 		JLabel picLabel = new JLabel(new ImageIcon(myPicture));
 		picLabel.setBounds(0, 0, viewPanel.getWidth(), viewPanel.getHeight());
 		return picLabel;
+	}
+	
+	/*
+	 * Just a simpler print method to print stuff to the consul
+	 */
+	private void printf(String s){
+		System.out.println(s);
+		
 	}
 
 }
