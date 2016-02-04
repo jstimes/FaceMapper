01d5fd97375657e74028909479a4c99a219c443a
Jacob Stimes <Jacob Stimes>
2016-02-04 12:32:57 -0600

    Flipped identify & train python files

--- src/python/identify_users.py
+++ src/python/identify_users.py
@@ -2,105 +2,78 @@
 
 # Import the required modules
 import cv2, os
-import numpy as np
 import sys
-import identify_users
+import json
+import numpy as np
 from PIL import Image
 
-#TODO possibly split this script into two functions,
-# one that takes a directory and another just a bunch of possibly random filepaths?
-def main():
-	try:
-		pathToImages = str(sys.argv[1])
-		pathToTrainedModel = os.path.join('src', 'python', 'TrainedModel')
-		#pathToTrainedModel = "src/python/TrainedModel"
-		user = str(sys.argv[2])
-	except IndexError:
-		print "Incorret syntax. Expecting python train_for_user.py PATHTOIMAGES USER"
-		return
+def identify(pathToTestImage,isMainScript,newImagePath):
+    
+    #TODO move this try block to the main method part
+    try:
+        #pathToTrainedModels = str(sys.argv[1])
+        pathToTrainedModel = os.path.join('src', 'python', 'TrainedModel')
+        #pathToTrainedModel = "/home/tweets/FaceMapper/src/python/TrainedModel"
+        #pathToTrainedModel = "src/python/TrainedModel"
+        #pathToTestImage = str(sys.argv[1])
+    except IndexError:
+        print "Incorret syntax. Expecting python identify_users.py pathToModel pathToSaveImage"
+        return
 
-	# For face detection we will use the Haar Cascade provided by OpenCV.
-	cascadePath = os.path.join('src', 'python', 'haarcascade_frontalface_default.xml')
-	faceCascade = cv2.CascadeClassifier(cascadePath)
+    # For face detection we will use the Haar Cascade provided by OpenCV.
+    cascadePath = os.path.join('src', 'python', 'haarcascade_frontalface_default.xml')
+    #cascadePath = "src/python/haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(cascadePath)
 
-	# For face recognition we will the the LBPH Face Recognizer 
-	recognizer = cv2.face.createLBPHFaceRecognizer()
-	
-	# Load a previously created model:
-	recognizer.load(pathToTrainedModel)
+    # For face recognition we will use the the LBPH Face Recognizer
+    recognizer = cv2.face.createLBPHFaceRecognizer()
+    print pathToTrainedModel
+    recognizer.load(pathToTrainedModel)
 
-	def get_images_and_labels(path, userLabel):
-		# Append all the absolute image paths in a list image_paths
-		image_paths = [os.path.join(path, f) for f in os.listdir(path)]
+    #Open image for prediction
+    predict_image_pil = Image.open(pathToTestImage).convert('L')
+    predict_image = np.array(predict_image_pil, 'uint8')
+    faces = faceCascade.detectMultiScale(predict_image, 1.3, 5)
+    if len(faces) == 0:
+        print "0 faces found"
+        return
+        
+    results = []
+    
+    img = cv2.imread(pathToTestImage, cv2.IMREAD_COLOR)
+    
+    #For each possible face in that image
+    for (x, y, w, h) in faces:
+        nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
+        #nbr_actual = int(os.path.split(image_path)[1].split(".")[0].replace("subject", "")) #Not testing at this time.
+        #print "We believe this face is", nbr_predicted, "with", conf, "confidence"
 
-		# images will contains face images
-		images = []
+		# Add label for each face found in image
+        label = "Unrecognized"
+        filePath = os.path.join('src' 'database' 'userNameLabelMapping')
+        with open(filePath) as data_file:    
+        	data = json.load(data_file)
+        	for i,val in enumerate(data):
+        		if data[i]["label"] == nbr_predicted:
+        			label = data[i]["name"]
+        			break
 
-		# labels will contains the label that is assigned to the image
-		labels = []
-		
-		# save last image to test training with
-		last = None 
-
-		imageCount = 0
-		totalFaces = 0
-		for image_path in image_paths:
-			imageCount = imageCount + 1
-			
-			if imageCount == len(image_paths):
-				last = image_path
-				break
-			
-			# Read the image and convert to grayscale
-			image_pil = Image.open(image_path).convert('L')
-
-			# Convert the image format into numpy array
-			image = np.array(image_pil, 'uint8')
-
-			nbr = int(userLabel)
-
-			# Detect the face in the image
-			faces = faceCascade.detectMultiScale(image, 1.3, 5)
-			if len(faces) == 0:
-				print "no faces detected in",image_path
-
-			faceCount = 0
-			# If face is detected, append the face to images and the label to labels
-			for (x, y, w, h) in faces:
-				faceCount = faceCount + 1
-				totalFaces = totalFaces + 1
-				images.append(image[y: y + h, x: x + w])
-				labels.append(nbr)
-				cv2.waitKey(1000)
-
-			#print "Found", faceCount, "faces in image", imageCount, "for user: ", nbr
-
-		# return the images list and labels list and last image for testing
-		return images, labels, last
-
-	# Path to the dataset
-	path = pathToImages
-
-	#Get the labels and images
-	images, labels, last = get_images_and_labels(path, user)
-	if len(images) < 2:
-		print "Error - need at least 2 images"
-		return
-	cv2.destroyAllWindows()
-
-	# Perform the training and update previous model
-	recognizer.update(images, np.array(labels))
-
-	# save model to be used & updated again
-	recognizer.save(pathToTrainedModel)
-	
-	# Test if model had enough data to recognize last picture in directory
-	test = identify_users.identify(last,False,"")
-	if len(test) > 0 and int(test[0]) == int(user):
-		print "success"
-	else:
-		print "failure"
+        if isMainScript:
+        	print nbr_predicted, conf
+        if nbr_predicted is None: 
+         #TODO handle this case..might not even happen
+         # because I've never seen it and the model may try to match the image no matter what
+         # but i haven't actually found any documentation to confirm this
+        	print "null"  
+         
+        #parameters are image, label-text, origin point for label, font, font-size, RGB text color, thickness
+        cv2.putText(img, label, (x,y+h), cv2.FONT_HERSHEY_PLAIN, 3, (243,74,32), 2);
+        results.append(nbr_predicted)
+        
+    if isMainScript:
+    	cv2.imwrite(newImagePath, img)
+    return results
 
 if __name__ == '__main__':
-	main()
+    identify(str(sys.argv[1]),True,sys.argv[2])
 
